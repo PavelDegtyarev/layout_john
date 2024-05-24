3f04a5fbfa62d1859e52dba5ff0a0dc6558a7437
PavelDegtyarev <pavel.ural@mail.ru>
2024-05-24 22:33:48 +0500

    create Home page

---
+++ js/script.js
@@ -0,0 +1,5 @@
+
+document.querySelector('.icon-menu').addEventListener('click', () => {
+    document.querySelector('.menu__body').classList.toggle('_active')
+    document.querySelector('.icon-menu').classList.toggle('_active')
+})
